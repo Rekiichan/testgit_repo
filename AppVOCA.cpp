9e5c961247fead5813f043d7c80031f4350d04fd
hi <nguyenminh121201@gmail.com>
2022-07-06 22:16:10 +0700

    first commit

---
+++ AppVOCA.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Word.h"
+
+using namespace std; 
+int main()
+{
+	Word voca;
+	voca.VOCAInit();
+	system("pause");
+	return 0;
+}
+
---
+++ Word.h
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Word
+{
+public:
+	int ID;
+	string ENG;
+	string VIET;
+	int Type;
+
+	Word() {
+	}
+
+	void VOCAInit();
+	void VOCAShow();
+};
